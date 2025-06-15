cf02d1985dfe75f81932f211cb4a2f7de8b21fc1
thammarongsak <134133829+thammarongsak@users.noreply.github.com>
2025-06-15 18:10:14 +0700

    Update main.py

--- main.py
+++ main.py
@@ -21,4 +21,5 @@ def index():
     return render_template("index.html", response=response)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
